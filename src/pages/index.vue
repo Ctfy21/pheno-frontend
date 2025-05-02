4a430a78f326c86966359385b0bdd42335111126
Ctfy21 <skileev@bk.ru>
2025-05-02 10:46:12 +0300

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar :elevation="2">
+    <template #prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title>Эксперименты ВНИИСБ</v-app-bar-title>
+    <template #append>
+      <v-btn icon="mdi-magnify" />
+      <v-btn icon="mdi-dots-vertical" />
+    </template>
+  </v-app-bar>
+
+  <v-container class="justify-center align-center">
+    <v-stepper :items="['Step 1', 'Step 2', 'Step 3']" max-width="800">
+
+    </v-stepper>
+  </v-container>
+
+</template>
+
+<script setup>
+  //
+</script>
